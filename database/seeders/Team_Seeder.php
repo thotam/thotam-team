0f5e081a15926172ab0a2f9e735fc6ba8054bcc1
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-09 16:51:38 +0700

    add Role and Permission

--- database/seeders/Team_Seeder.php
+++ database/seeders/Team_Seeder.php
@@ -3,6 +3,9 @@
 namespace Thotam\ThotamTeam\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Thotam\ThotamTeam\Models\PhanLoaiNhom;
 
 class Team_Seeder extends Seeder
 {
@@ -59,5 +62,83 @@ class Team_Seeder extends Seeder
                 'active' => true
             ]
         );
+
+        //Role and Permission
+        $permission[] = Permission::updateOrCreate([
+            'name' => 'view-team'
+        ],[
+            "description" => "Xem Nhóm",
+            "group" => "Team",
+            "order" => 1,
+            "lock" => true,
+        ]);
+
+        $permission[] = Permission::updateOrCreate([
+            'name' => 'add-team'
+        ],[
+            "description" => "Thêm Nhóm",
+            "group" => "Team",
+            "order" => 2,
+            "lock" => true,
+        ]);
+
+        $permission[] = Permission::updateOrCreate([
+            'name' => 'edit-team'
+        ],[
+            "description" => "Sửa Nhóm",
+            "group" => "Team",
+            "order" => 3,
+            "lock" => true,
+        ]);
+
+        $permission[] = Permission::updateOrCreate([
+            'name' => 'delete-team'
+        ],[
+            "description" => "Xóa Nhóm",
+            "group" => "Team",
+            "order" => 4,
+            "lock" => true,
+        ]);
+
+        $permission[] = Permission::updateOrCreate([
+            'name' => 'set-member-team'
+        ],[
+            "description" => "Set nhân viên cho Nhóm",
+            "group" => "Team",
+            "order" => 5,
+            "lock" => true,
+        ]);
+
+        $super_admin = Role::updateOrCreate([
+            'name' => 'super-admin'
+        ],[
+            "description" => "Super Admin",
+            "group" => "Admin",
+            "order" => 1,
+            "lock" => true,
+        ]);
+
+        $admin = Role::updateOrCreate([
+            'name' => 'admin'
+        ],[
+            "description" => "Admin",
+            "group" => "Admin",
+            "order" => 2,
+            "lock" => true,
+        ]);
+
+        $admin_team = Role::updateOrCreate([
+            'name' => 'admin-team'
+        ],[
+            "description" => "Admin Team",
+            "group" => "Admin",
+            "order" => 4,
+            "lock" => true,
+        ]);
+
+        $super_admin->givePermissionTo($permission);
+        $admin->givePermissionTo($permission);
+        $admin_team->givePermissionTo($permission);
+
     }
 }
