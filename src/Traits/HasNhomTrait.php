83b0b994744b8db876b3a3705e42b003bd16a2e6
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-04-12 17:28:33 +0700

    Update HasNhomTrait.php

--- src/Traits/HasNhomTrait.php
+++ src/Traits/HasNhomTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Thotam\ThotamPlus\Traits;
+namespace Thotam\ThotamTeam\Traits;
 
 use Thotam\ThotamTeam\Models\Nhom;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -8,23 +8,23 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 trait HasNhomTrait {
 
     /**
-     * The quanly_of_nhom that belong to the HasNhomTrait
+     * The quanly_of_nhoms that belong to the HasNhomTrait
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function quanly_of_nhom(): BelongsToMany
+    public function quanly_of_nhoms(): BelongsToMany
     {
-        return $this->belongsToMany(Nhom::class, 'nhom_quanly_table', 'hr_key', 'nhom_id');
+        return $this->belongsToMany(Nhom::class, 'nhom_quanlys', 'hr_key', 'nhom_id');
     }
 
     /**
-     * The thanhvien_of_nhom that belong to the HasNhomTrait
+     * The thanhvien_of_nhoms that belong to the HasNhomTrait
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function thanhvien_of_nhom(): BelongsToMany
+    public function thanhvien_of_nhoms(): BelongsToMany
     {
-        return $this->belongsToMany(Nhom::class, 'nhom_thanhvien_table', 'hr_key', 'nhom_id');
+        return $this->belongsToMany(Nhom::class, 'nhom_thanhviens', 'hr_key', 'nhom_id');
     }
 
 }
